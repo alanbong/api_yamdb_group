2a182efd819914077eae49b42862cbd30a96e06e
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2024-11-26 15:44:28 +0400

    add DEFAULT_FROM_EMAIL to settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,6 +114,7 @@ AUTH_USER_MODEL = 'reviews.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+DEFAULT_FROM_EMAIL = 'noreply@yamdb.com'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
