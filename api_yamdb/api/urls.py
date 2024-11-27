45f4f1ab1fbce8cb529b326438e554ce129c7ffb
Даулетназар Мамбетназаров <richdad95@yandex.ru>
2024-11-27 11:35:07 +0300

    daulet-5 urls исправил

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,27 @@
 from django.urls import path
-from .views import SignupView, TokenView
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    CategoryViewSet,
+    GenreViewSet,
+    TitleViewSet,
+    CommentViewSet,
+    ReviewViewSet,
+    SignupView,
+    TokenView
+)
+
+v1_router = DefaultRouter()
+v1_router.register('categories', CategoryViewSet, basename='category')
+v1_router.register('genres', GenreViewSet, basename='genre')
+v1_router.register('titles', TitleViewSet, basename='title')
+
+v1_router.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='title-reviews')
+
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='title-comments')
 
 urlpatterns = [
     path('auth/signup/', SignupView.as_view(), name='signup'),
