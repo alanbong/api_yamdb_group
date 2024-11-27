542a6829aa060f140cf71186cb152852ce4e2a96
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2024-11-27 15:21:12 +0400

    add CustomUser to auth app

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role', 'bio')  # поля для отображения
-    list_filter = ('role',)  # Фильтрация по ролям
-    search_fields = ('username', 'email')  # Поиск по имени и email
+class CustomUserAdmin(UserAdmin):
+    """Настройка отображения кастомной модели пользователя в админке."""
+    list_display = ('username', 'email', 'role', 'bio')
+    list_filter = ('role',)
+    search_fields = ('username', 'email')
+
+
+User._meta.app_label = "auth"
